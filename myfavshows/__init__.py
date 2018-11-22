4e990cc599da64a6d4459c71a7dbaafd3d70fdf7
Adrienth <adrien.thomas.si@gmail.com>
2018-11-22 13:28:33 +0100

    disabled the HTTPonly flag for cookies to have the "document.cookie" flag work

--- myfavshows/__init__.py
+++ myfavshows/__init__.py
@@ -9,6 +9,9 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'myfavshows.sqlite'),
+        SESSION_COOKIE_HTTPONLY=False,
+        REMEMBER_COOKIE_HTTPONLY=False,
+        SESSION_COOKIE_SECURE=False,
     )
 
     if test_config is None:
